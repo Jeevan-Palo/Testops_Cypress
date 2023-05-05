bbba61c3a0489402b8e765b0d84569076796fb99
JeevanRadhakrishnan <jradhakrishnan@palo-it.com>
2023-05-05 13:44:45 +0800

    first commit

---
+++ cypress/integration/dashboard_spec.js
@@ -0,0 +1,7 @@
+describe('Testing the hero dashboard page', function() {
+
+  it('should contain the word heroes', function() {
+    cy.visit('https://www.google.com/');
+  })
+
+});
---
+++ cypress/integration/heroes_list_spec.js
@@ -0,0 +1,8 @@
+describe('Testing the hero list page', function() {
+
+  it('should have heroes in the list', function() {
+    cy.visit('https://www.google.com/');
+
+  });
+
+});
